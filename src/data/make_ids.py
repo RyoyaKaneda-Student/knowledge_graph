e2e4b35dc167ace99c8adf20a783313198c62c5c
RyoyaKanedaStudent <kaneda@ss.cs.osakafu-u.ac.jp>
2022-06-15 15:03:28 +0900

    keizoku

--- src/data/make_ids.py
+++ src/data/make_ids.py
@@ -22,16 +22,75 @@ def is_integer(n):
         return float(n).is_integer()
 
 
-def function01(usetitle):
+def make_g(usetitle):
     g = Graph()
     g.parse(f'data/external/2020v2/{usetitle}.ttl')
     g.bind(usetitle, f"http://kgc.knowledge-graph.jp/data/{usetitle}/", override=True)
-
-    def change_name(s, p, o):
-        return s.n3(g.namespace_manager), p.n3(g.namespace_manager), o.n3(g.namespace_manager)
-
     prefix_dict = {item[0]: Namespace(item[1]) for item in g.namespaces()}
     prefix_dict = AttrDict(prefix_dict)
+    return g, prefix_dict
+
+
+def change_name(g, s, p, o):
+    return s.n3(g.namespace_manager), p.n3(g.namespace_manager), o.n3(g.namespace_manager)
+
+
+def make_ERdicts(g, Edict=None, Rdict=None):
+    if Edict is None:
+        Edict = {}
+    if Rdict is None:
+        Rdict = {}
+    Eflist, Rlist, Etlist = [], [], []
+
+    for s, p, o in g.triples((None, None, None)):
+        s, p, o = change_name(g, s, p, o)
+        Eflist.append(s)
+        Rlist.append(p)
+        Etlist.append(o)
+
+    Elist = Eflist + Etlist
+    # 重複消去
+    Elist = sorted(set(Elist), key=Elist.index)
+    Rlist = sorted(set(Rlist), key=Rlist.index)
+    # id付
+    lenEdict, lenRdict = len(Edict), len(Rdict)
+    Edict = Edict.update({i+lenEdict: l for i, l in enumerate(Elist)})
+    Rdict = Rdict.update({i+lenRdict: l for i, l in enumerate(Rlist)})
+    return Edict, Rdict
+
+
+def make_triple(g, Edict, Rdict):
+    # triple に関する処理
+    triples = []
+    tmp01, tmp02 = {}, {}
+    for s, p, o in g.triples((None, None, None)):
+        s, p, o = s.n3(g.namespace_manager), p.n3(g.namespace_manager), o.n3(g.namespace_manager)
+        _s, _p, _o = Edict[s], Rdict[p], Edict[o]
+
+        triples.append((_s, _p, _o))
+
+    for s in range(max(Edict.values()) + 1):
+        for p in range((max(Rdict.values()) + 1) * 2):
+            tmp01[(s, p)] = []
+            tmp02[(s, p)] = []
+
+    return triples, tmp01, tmp02
+
+
+def make_():
+    # source, label に関する処理
+    sources = {}
+    labels = {}
+    for s, p, o in g.triples((None, None, None)):
+        s, p, o = s.n3(g.namespace_manager), p.n3(g.namespace_manager), o.n3(g.namespace_manager)
+        _s, _p, _o = Ef[s], R[p], Et[o]
+        if p == "kgc:source" and '@en' in o:
+            sources[_o] = o
+        elif p == "rdfs:label":
+            labels[_o] = o
+
+def function01(usetitle):
+    g, prefix_dict = make_g(usetitle)
 
     Ef, R, Et = {}, {}, {}
 
@@ -57,6 +116,9 @@ def function01(usetitle):
     tmp = list(set(list(Ef.items()) + list(Et.items())))
     for key, value in sorted(tmp, key=lambda x: x[0]):
         revE[value] = key
+    revR = {}
+    for key, value in R.items():
+        revR[value] = key
 
     # triple に関する処理
     triples = []
@@ -112,6 +174,58 @@ def function01(usetitle):
     with open(f"data/processed/{usetitle}/test.pickle", "wb") as f:
         pickle.dump(df.values, f)
 
+    return Ef, R, Et, revE, revR, triples
+
+
+def function02(usetitle):
+    g, prefix_dict = make_g(usetitle)
+
+    Edict, Rdict = make_ERdicts(g)
+
+    triples, tmp01, tmp02 = make_triple(g, Edict, Rdict)
+
+    # source, label に関する処理
+    sources = {}
+    labels = {}
+    for s, p, o in g.triples((None, None, None)):
+        s, p, o = s.n3(g.namespace_manager), p.n3(g.namespace_manager), o.n3(g.namespace_manager)
+        _s, _p, _o = Ef[s], R[p], Et[o]
+        if p == "kgc:source" and '@en' in o:
+            sources[_o] = o
+        elif p == "rdfs:label":
+            labels[_o] = o
+
+    # Story に関する処理
+    stories = []
+    s_taiou = {}  # [index] = (new s)
+    for s, p, o in g.triples((None, RDF.type, None)):
+        s, p, o = change_name(s, p, o)
+        if o in ("kgc:Situation", "kgc:Statement", "kgc:Thought"):
+            # print(s, p, o)
+            result = re.findall(fr'({usetitle}:)(\d+)([a-z]?)', s)
+            if len(result) == 0:
+                continue
+            result = result[0]
+            s_ = result[0] + result[1].zfill(4) + result[2]
+            s_taiou[Ef[s]] = s_
+            stories.append(s_)
+
+    stories = sorted(stories)
+    print(stories)
+
+    df = pd.DataFrame(triples)
+    df.to_csv(f"data/processed/{usetitle}/ids.tsv", header=False, index=False, sep='\t')
+    with open(f"data/processed/{usetitle}/to_skip.pickle", "wb") as f:
+        pickle.dump({'lhs': tmp01, 'rhs': tmp02}, f)
+    with open(f"data/processed/{usetitle}/train.pickle", "wb") as f:
+        pickle.dump(df.values, f)
+    with open(f"data/processed/{usetitle}/valid.pickle", "wb") as f:
+        pickle.dump(df.values, f)
+    with open(f"data/processed/{usetitle}/test.pickle", "wb") as f:
+        pickle.dump(df.values, f)
+
+    return Ef, R, Et, revE, revR, triples
+
 
 def main():
     titles = {
@@ -124,8 +238,10 @@ def main():
         "白銀号事件": "SilverBlaze",
         "マダラのひも": "SpeckledBand"
     }
+
+    dict_ = {}
     for jaName, tagName in titles.items():
-        function01(tagName)
+        dict_[tagName] = function01(tagName)
     print("complete")
 
 
